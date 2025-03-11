da3c32453ac87786fccf506ffc4f8db23d37d3e7
Julio Sanchez G <je02sg@gmail.com>
2025-03-11 16:41:18 +0100

    fix: InfoView adding Comments for Localization

--- Ombrace/Ombrace/Views/infoview.swift
+++ Ombrace/Ombrace/Views/infoview.swift
@@ -12,7 +12,7 @@ struct InfoView: View {
                     .frame(width: 100, height: 100)
                 
                 
-                Text("Welcome to Ombrace!")
+                Text("Welcome to Ombrace!", comment: "Ombrace is the name of the app, is a welcoming title")
                     .font(.title2)
                     .fontWeight(.bold)
                     .foregroundColor(.primary)
@@ -20,30 +20,30 @@ struct InfoView: View {
                     .padding(.horizontal)
                 
                 Group {
-                    Text("A space designed to support individuals who are navigating body dysmorphia with self-awareness and a desire for change.")
+                    Text("A space designed to support individuals who are navigating body dysmorphia with self-awareness and a desire for change.", comment: "Information about the app")
                     
                     Divider().padding(.horizontal, 40)
                     
-                    Text("Our mission is to help you cultivate a healthier connection with your body through self-compassion, body-focused exercises, and mindful practices.")
+                    Text("Our mission is to help you cultivate a healthier connection with your body through self-compassion, body-focused exercises, and mindful practices.", comment: "Information about the app")
                     
                     Divider().padding(.horizontal, 40)
                     
-                    Text("Inspired by research on self-compassion and supportive touch, our approach encourages gentle, non-judgmental engagement with your body, helping you shift towards a more balanced and appreciative self-perception.")
+                    Text("Inspired by research on self-compassion and supportive touch, our approach encourages gentle, non-judgmental engagement with your body, helping you shift towards a more balanced and appreciative self-perception.", comment: "Information about the app")
                     
                     Divider().padding(.horizontal, 40)
                     
-                    Text("While we are not medical professionals, our methods are rooted in scientific studies that emphasize kindness, acceptance, and connection as tools for well-being.")
+                    Text("While we are not medical professionals, our methods are rooted in scientific studies that emphasize kindness, acceptance, and connection as tools for well-being.", comment: "Information about the app")
                     
                     Divider().padding(.horizontal, 40)
                     
-                    Text("This journey is personal, and progress looks different for everyone. Whether you're taking your first step or deepening your practice, Ombrace is here to guide you in fostering a more positive and compassionate relationship with yourself.")
+                    Text("This journey is personal, and progress looks different for everyone. Whether you're taking your first step or deepening your practice, Ombrace is here to guide you in fostering a more positive and compassionate relationship with yourself.", comment: "Information about the app")
                 }
                 .font(.body)
                 .foregroundColor(.secondary)
                 .multilineTextAlignment(.center)
                 .padding(.horizontal, 20)
                 
-                Text("You are worthy of kindness—especially from yourself.")
+                Text("You are worthy of kindness—especially from yourself.", comment: "Information about the app")
                     .font(.headline)
                     .fontWeight(.semibold)
                     .foregroundColor(.accent2)
